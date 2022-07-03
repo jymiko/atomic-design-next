7ff19fbc10ef5ff7ebfb3e89577bebfc16fd9c01
jymiko <jymiko92@gmail.com>
2022-07-03 22:59:31 +0700

    add atomic component link

---
+++ components/atoms/Link/index.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import classnames from 'classnames'
+
+import styles from './style.module.scss'
+
+type Props = {
+    className: string,
+    text: string,
+    href: string
+} 
+
+const LinkComponent = (props:Props) => {
+
+    return(
+        <Link href={'/'+props.href}>
+            <span className={classnames(styles.title, props.className)}>{props.text}</span>
+        </Link>
+    )
+}
+
+LinkComponent.defaultProps = {
+    className: '',
+    text: '',
+    href: '#'
+}
+
+export default LinkComponent
